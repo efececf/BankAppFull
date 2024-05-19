c91a87cac54dfa01f6c9cd269e3e3d2d90591009
Efe Ece <94974319+efececf@users.noreply.github.com>
2024-05-19 23:26:50 +0300

    Add files via upload

---
+++ AccountMove.java
@@ -0,0 +1,41 @@
+package Models;
+import java.time.LocalDateTime;
+public class AccountMove {
+    private int hareketCinsiID;
+    private int hareketNo;
+    private String hareketAciklamasi;
+    private LocalDateTime hareketZamani;
+    private String affectedAccount;
+    private String accOwner;
+    private double effectOfMove;
+    public AccountMove(int hareketCinsiID,double effectOfMove,String hareketAciklamasi,String accOwner,LocalDateTime hareketZamani,String affectedAccount){
+        this.hareketCinsiID=hareketCinsiID;
+        this.effectOfMove=effectOfMove;
+        this.hareketZamani=hareketZamani;
+        this.affectedAccount=affectedAccount;
+        this.hareketAciklamasi=hareketAciklamasi;
+        this.accOwner=accOwner;
+        
+    }
+    public String showAccOw(){
+        return this.accOwner;
+    }
+    public String getAciklama(){
+        return this.hareketAciklamasi;
+    }
+    public int getHareketCinsi(){
+        return this.hareketCinsiID;
+    }
+    
+    public LocalDateTime showHareketZamani(){
+        return this.hareketZamani;
+    }
+    public double showEffect(){
+        return effectOfMove;
+    }
+    public String showAffectedAccountUser(){
+        return affectedAccount;
+    }
+
+
+}
